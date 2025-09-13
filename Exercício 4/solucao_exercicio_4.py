e481443cfcc2ffa99926af957d45744f9f76c8a5
Victor <vitor.cynh@gmail.com>
2025-09-13 20:52:58 -0300

    Delete Exercício 4 directory

--- Exercício 4/solucao_exercicio_4.py
+++
@@ -1,7 +0,0 @@
-numeros_originais = [1, 2, 3, 4, 5, 6, 7, 8]
-numeros_pares = []
-for numero in numeros_originais:
-    if numero % 2 == 0:
-        numeros_pares.append(numero)
-print(f"Lista original: {numeros_originais}")
-print(f"Nova lista com números pares: {numeros_pares}")
