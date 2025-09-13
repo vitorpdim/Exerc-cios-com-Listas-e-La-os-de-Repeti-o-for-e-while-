9a460b60d606bfbe14af6a5189de781413dfd5ee
Victor <vitor.cynh@gmail.com>
2025-09-13 20:52:41 -0300

    Delete Exercício 3 directory

--- Exercício 3/Ex_03.py
+++
@@ -1,8 +0,0 @@
-'''
-Crie uma lista com 5 nomes de cidades e use um laço for para imprimir cada
-nome.
-'''
-cidades = ["São Paulo", "Guaratinguetá", "Lorena", "Aparecida", ""]
-
-for cidade in cidades:
-    print(cidade)
